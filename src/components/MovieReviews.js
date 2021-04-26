ce116930dee325dad9d96461f9461d221336a5bc
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-25 20:12:02 -0500

    commit tree 1d3173d276f3cde87321f90e5fa69dc3e54bb63f

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,19 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const Review = ({ headline, summary_short}) =>
+    <div className={"review"}>
+        <h3>{headline}</h3>
+        <p>{summary_short}</p>
+    </div>
+
+const MovieReviews = ({reviews}) =>
+    <div className={"review-list"}>
+        {this.props.reviews.map(Review)}
+    </div>
+
+MovieReviews.defaultProps = {
+    reviews: []
+}
+
+export default MovieReviews
